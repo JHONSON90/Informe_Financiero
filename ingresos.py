d7f3639087468ae1e78e3f2782c36ca186eadaab
Jhonson90 <edisonportillal@gmail.com>
2025-11-05 18:06:31 -0500

    Haciendo distribucion de la capita para mayor claridad del informe revisar si se puede hacer con la facturacion de medicamentos

--- ingresos.py
+++ ingresos.py
@@ -6,6 +6,53 @@ nombre_archivo = "Ingresos_clasificados.xlsx"
 
 archivo = os.path.join(ruta, nombre_archivo)
 data = pl.read_excel(archivo, sheet_name="Ingresos", schema_overrides={"1" : pl.Int64, "Total general" : pl.Int64})
+
+capita = data.filter(
+    pl.col("CuentasMov") == "4110102702"
+)
+
+
+distribucion_porcentual = pl.DataFrame({
+    "CuentasMov":['4110102102','4110102602','4110102702','4110102802','4110103302','4110103402','4110104202','4110104402','4110105402','4125100216','4125100310','4130100102','4130100202','4130100602'],
+    "Porcentaje":[0.00858850720267853,0.0129675414397561,0.546802357534043,0.0148633152315544,0.00453751774427452,0.0376729843659472,0.00518037293294954,0.0083753276786851,0.308454237545001,0.0079969713590606,0.0302921099976902,0.00553462752014605,0.00329110823991143,0.00544302120830266]
+})
+ 
+capita = capita.with_columns(
+    pl.struct(pl.all()).rank(method="ordinal").alias("Rango")
+)
+
+capita = capita.join(distribucion_porcentual, how="cross")
+capita = capita.with_columns(
+    (pl.col("Total general")*pl.col("Porcentaje")).alias("Valor Personalizado").cast(pl.Int64, strict=True)
+).select(
+    "d_mes",
+    "CuentasMov_right",
+    "1",
+    "Valor Personalizado"
+)
+
+cambiar_nombres = {
+    "d_mes" : "d_mes",
+    "CuentasMov_right" : "CuentasMov",
+    "1" : "1",
+    "Valor Personalizado" : "Total general"
+}
+
+capita = capita.rename(cambiar_nombres)
+
+print(capita)
+
+data = data.filter(
+    pl.col("CuentasMov") != "4110102702"
+)
+
+data = pl.concat([data, capita])
+
+print(data)
+
+
+
+
 clasificacion = pl.read_excel(archivo, sheet_name="Clasificaciones")
 #, schema_overrides={"ORDEN" : pl.Int64,})
 
@@ -51,4 +98,4 @@ nuevos_nombres = {
 
 ingresos = ingresos.rename(nuevos_nombres)
 
-print(ingresos)
+#print(ingresos)
